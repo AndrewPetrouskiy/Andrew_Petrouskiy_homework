a9d681cdf0d7acd572d5b0f0493a914bee6f20ce
KeNnY <kenny_0@mail.ru>
2022-08-06 20:16:28 +0300

    my work of the first seminar

---
+++ work/Program.cs
@@ -0,0 +1,94 @@
+﻿//Task 1
+
+// Console.Write("Enter first number ");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Enter second number ");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+
+// if(number1 == number2 * number2){
+//     Console.WriteLine($"{number1} is square of {number2}");
+// }
+// else
+// {
+//     Console.WriteLine($"{number1} isn't square of {number2}");
+// }
+
+
+// Task 2. first solution 
+
+// Console.Write("Enter number which will be a day of a week ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// if(number == 1){
+//    Console.Write($"{number} is a Monday"); 
+// }
+// if(number == 2){
+//    Console.Write($"{number} is a Tuesday"); 
+// }
+// if(number == 3){
+//    Console.Write($"{number} is a Wednesday"); 
+// }
+// if(number == 4){
+//    Console.Write($"{number} is a Thursday"); 
+// }
+// if(number == 5){
+//    Console.Write($"{number} is a Friday"); 
+// }
+// if(number == 6){
+//    Console.Write($"{number} is a Saturday"); 
+// }
+// if(number == 7){
+//    Console.Write($"{number} is a Sunday"); 
+// }
+// if(number > 7){
+//    Console.Write($"{number} isn't equal a day of a week"); 
+// }
+
+// Task 2. Second solution
+
+// Console.Write("Enter number which will be a day of a week ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// switch(number){
+//     case 1:
+//         Console.Write($"{number} is a Monday"); 
+//     break;
+//     case 2:
+//         Console.Write($"{number} is a Tuesday");  
+//     break;
+//     case 3:
+//         Console.Write($"{number} is a Wednesday"); 
+//     break;
+//     case 4:
+//         Console.Write($"{number} is a Thursday"); 
+//     break;
+//     case 5:
+//         Console.Write($"{number} is a Friday"); 
+//     break;
+//     case 6:
+//         Console.Write($"{number} is a Saturday"); 
+//     break;
+//     case 7:
+//         Console.Write($"{number} is a Sunday"); 
+//     break;
+// }
+
+
+//Task 3
+
+// Console.Write("Enter a number ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// for(int i = -n; i <= n; i++){
+//     Console.Write(i);
+//     Console.Write(", ");
+// }
+
+// Task 4
+
+// Console.Write("Enter a number ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int remainder = number % 10;
+
+// Console.Write($" {remainder} is remainder of the number {number}");
+
