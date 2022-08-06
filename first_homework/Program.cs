4a8cff122d16122776265377e7c8056a34548ff4
KeNnY <kenny_0@mail.ru>
2022-08-06 20:46:07 +0300

    Task 3

--- first_homework/Program.cs
+++ first_homework/Program.cs
@@ -1,4 +1,4 @@
-﻿//Task 1
+﻿// Task 1
 
 // Console.Write("Enter the first number ");
 // int number1 = Convert.ToInt32(Console.ReadLine());
@@ -16,16 +16,30 @@
 
 // Task 2
 
-Console.Write("Enter the first number ");
-int number1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Enter the second number ");
-int number2 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Enter the third number ");
-int number3 = Convert.ToInt32(Console.ReadLine());
-int Max = number1;
+// Console.Write("Enter the first number ");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Enter the second number ");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Enter the third number ");
+// int number3 = Convert.ToInt32(Console.ReadLine());
+// int Max = number1;
 
-if(number1 > Max) Max = number1;
-if(number2 > Max) Max = number2;
-if(number3 > Max) Max = number3;
+// if(number1 > Max) Max = number1;
+// if(number2 > Max) Max = number2;
+// if(number3 > Max) Max = number3;
 
-Console.WriteLine($"{Max} is MAX");
+// Console.WriteLine($"{Max} is MAX");
+
+
+// Task 3
+
+Console.Write("Enter a number ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number % 2 == 0){
+    Console.WriteLine($"{number} is EVEN");
+    }
+else
+{
+    Console.WriteLine($"{number} is ODD");
+}
