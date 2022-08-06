20973d5206607cb59ce1c825e2322f8ac70a3813
KeNnY <kenny_0@mail.ru>
2022-08-06 20:42:31 +0300

    Task 2

--- first_homework/Program.cs
+++ first_homework/Program.cs
@@ -1,14 +1,31 @@
 ﻿//Task 1
 
+// Console.Write("Enter the first number ");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Enter the second number ");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+
+// if(number1 > number2){
+//     Console.WriteLine($"{number1} is MAX");
+//     }
+// else
+// {
+//     Console.WriteLine($"{number2} is MAX");
+// }
+
+
+// Task 2
+
 Console.Write("Enter the first number ");
 int number1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Enter the second number ");
 int number2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the third number ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+int Max = number1;
 
-if(number1 > number2){
-    Console.WriteLine($"{number1} is MAX");
-    }
-else
-{
-    Console.WriteLine($"{number2} is MAX");
-}
+if(number1 > Max) Max = number1;
+if(number2 > Max) Max = number2;
+if(number3 > Max) Max = number3;
+
+Console.WriteLine($"{Max} is MAX");
