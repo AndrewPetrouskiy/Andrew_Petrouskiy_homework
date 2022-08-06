98faa10c857d1eda4c12e0bbcdbfbd5379c515ab
KeNnY <kenny_0@mail.ru>
2022-08-06 20:29:27 +0300

    Task 1

---
+++ first_homework/Program.cs
@@ -0,0 +1,14 @@
+﻿//Task 1
+
+Console.Write("Enter the first number ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the second number ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if(number1 > number2){
+    Console.WriteLine($"{number1} is MAX");
+    }
+else
+{
+    Console.WriteLine($"{number2} is MAX");
+}
