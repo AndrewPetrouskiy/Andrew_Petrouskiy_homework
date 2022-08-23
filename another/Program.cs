abb4f41d367c2ec413e8aaee68ae7382aa397a3c
KeNnY <kenny_0@mail.ru>
2022-08-23 19:37:27 +0300

    Additional materials

---
+++ another/Program.cs
@@ -0,0 +1,46 @@
+﻿
+// int[] a = {1, 2, 3, 4, 2, 6, 4, 8, 9, 14, 11, 3, 0};
+
+// void PrintArray(int[] arr)
+// {
+// for(int i = 0; i < arr.Length; i++)
+// {
+//     System.Console.Write($"{arr[i]} ");
+// }
+// }
+
+
+// int[] FillArray(int[] arr)
+// {
+// Random random =  new Random();
+//     for(int i = 0; i < arr.Length; i++)
+//     {
+//         arr[i] = random.Next(100, 1000);
+
+//     }
+//     return arr;
+// }
+
+
+
+
+
+// void SortArray(int[] arg) 
+// {
+//    Array.Reverse(arg);
+//    foreach (var n in arg)
+//    {
+//     System.Console.Write($"{n} ");
+//    }
+// }
+
+// void ReverseMethod (int[] arg)
+// {
+//     Array.Sort(arg);
+//     foreach (var n in arg)
+//    {
+//     System.Console.Write($"{n} ");
+//    }
+// }
+
+
