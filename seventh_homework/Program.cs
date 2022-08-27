185b76c173162d3a90a8e2895286baa0f22ccb06
KeNnY <kenny_0@mail.ru>
2022-08-28 00:18:30 +0300

    add my seventh homework 2

--- seventh_homework/Program.cs
+++ seventh_homework/Program.cs
@@ -50,6 +50,82 @@
 //Задача 50. Напишите программу, которая на вход принимает число и ищет в двумерном массиве, 
 //и возвращает индексы этого элемента или же указание, что такого элемента нет.
 
+// int[,] FillArray2D(int m, int n, int minValue, int maxValue)
+// {
+//     int[,] array = new int[m, n];
+
+//     Random random = new Random();
+
+//     for (int i = 0; i < m; i++)
+//     {
+//         for (int j = 0; j < n; j++)
+//         {
+//             array[i, j] = random.Next(minValue, maxValue);
+//         }
+//     }
+//     return array;
+// }
+
+
+// void Print2dArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             System.Console.Write($"{array[i, j]} ");
+//         }
+//         System.Console.WriteLine();
+//     }
+// }
+
+// string CheckTheNumberInArray(int[,] arr, int number)
+// {
+
+//     string indexes = string.Empty;
+//     for (int i = 0; i < arr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < arr.GetLength(1); j++)
+//         {
+//             if (number == arr[i, j])
+//             {
+//                 indexes = indexes + i + "," + j+ ";  ";
+//             }
+//         }
+//     }
+//     return indexes;
+// }
+
+// System.Console.WriteLine("Enter the length number");
+// int lengthMassive = int.Parse(Console.ReadLine());
+// System.Console.WriteLine("Enter the higher number");
+// int higherMassive = int.Parse(Console.ReadLine());
+// System.Console.WriteLine("Enter the min number");
+// int minValue = int.Parse(Console.ReadLine());
+// System.Console.WriteLine("Enter the max number");
+// int maxValue = int.Parse(Console.ReadLine());
+
+// System.Console.WriteLine("Enter the number which you want to find in the array");
+// int number = int.Parse(Console.ReadLine());
+
+// int[,] array = FillArray2D(lengthMassive, higherMassive, minValue, maxValue);
+// Print2dArray(array);
+// System.Console.WriteLine();
+// string indexes = CheckTheNumberInArray(array , number);
+
+// if (indexes == string.Empty)
+// {
+//     System.Console.WriteLine($"The number {number} doesn't exist in this array");
+// }
+// else
+// {
+//     System.Console.WriteLine($"The number {number} is in array under indexes {indexes}");
+// }
+
+//===========================================================================================================
+//Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+
 int[,] FillArray2D(int m, int n, int minValue, int maxValue)
 {
     int[,] array = new int[m, n];
@@ -66,6 +142,15 @@ int[,] FillArray2D(int m, int n, int minValue, int maxValue)
     return array;
 }
 
+void PrintArray(int[] arr)
+{
+    
+    for (int i = 0; i < arr.Length; i++)
+    {
+        System.Console.Write($"{arr[i]} ");
+    }
+    
+}
 
 void Print2dArray(int[,] array)
 {
@@ -79,21 +164,28 @@ void Print2dArray(int[,] array)
     }
 }
 
-string CheckTheNumberInArray(int[,] arr, int number)
-{
 
-    string indexes = string.Empty;
-    for (int i = 0; i < arr.GetLength(0); i++)
+int[] CalculateSumNumberInColumn(int[,] array, int lengthMassive)
+{
+    int[] sumColumn = new int[lengthMassive];
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-        for (int j = 0; j < arr.GetLength(1); j++)
+        for (int j = 0; j < array.GetLength(1); j++)
         {
-            if (number == arr[i, j])
-            {
-                indexes = indexes + i + "," + j+ ";  ";
-            }
+            sumColumn[i] += array[j, i];
         }
+        
     }
-    return indexes;
+    return sumColumn;
+}
+
+void CalculateAverageNumberInColumn(int[] array, int lengthMassive)
+{
+int[] avrColumn = new int[lengthMassive];
+    foreach (double element in array)
+    {
+        System.Console.Write($"{element / lengthMassive}; "); 
+    }
 }
 
 System.Console.WriteLine("Enter the length number");
@@ -105,20 +197,12 @@ int minValue = int.Parse(Console.ReadLine());
 System.Console.WriteLine("Enter the max number");
 int maxValue = int.Parse(Console.ReadLine());
 
-System.Console.WriteLine("Enter the number which you want to find in the array");
-int number = int.Parse(Console.ReadLine());
-
 int[,] array = FillArray2D(lengthMassive, higherMassive, minValue, maxValue);
 Print2dArray(array);
 System.Console.WriteLine();
-string indexes = CheckTheNumberInArray(array , number);
-
-if (indexes == string.Empty)
-{
-    System.Console.WriteLine($"The number {number} doesn't exist in this array");
-}
-else
-{
-    System.Console.WriteLine($"The number {number} is in array under indexes {indexes}");
-}
-
+int[] sumColumn = CalculateSumNumberInColumn(array, lengthMassive);
+System.Console.Write("The sum of wich column in the array equals  ");
+PrintArray(sumColumn);
+System.Console.WriteLine();
+System.Console.Write("The average of columns in the array is equals  ");
+CalculateAverageNumberInColumn(sumColumn, lengthMassive);
